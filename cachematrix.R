1913ed01e0770deffe5c69c3677819b39c1c995b
bbbb619 <kellie.b.hudson@gmail.com>
2015-09-26 20:57:07 -0700

    Update cachematrix.R
    Completing homework 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,20 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Create null value m. 
 
 makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) m <<- inverse
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 
 }
 
@@ -12,4 +23,16 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  ## Checking to see if there is cached data.
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  #Use qr.solve() to get inverse of the matrix.
+  m <- qr.solve(data, ...)
+  x$setinverse(m)
+  m
 }
+
